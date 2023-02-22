caf75ee5a6798512b43e7882f36bf71972c115a0
AhmidouA <ahmedakkioui75@gmail.com>
2023-02-22 08:47:29 +0100

    Project - Session

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const {oilRouter, userRouter} = require('./app/routers');
+const {oilRouter, userRouter} = require('./server/routers');
 
-
+// formatage de données envoyées à un serveur 
 app.use(express.urlencoded({extended:true}));
 // app.use(express.json());
 
-
+// Le port du serveur
 const PORT = process.env.PORT ?? 3000;
 
+// utilisation des views pour le client (TEST)
 app.set('view engine', 'ejs');
-app.set('views', './app/views')
+app.set('views', './server/views')
+
+// middleware Session
+const session = require('express-session');
+app.use(
+    session({
+        saveUninitialized: true,
+        resave: true,
+        secret: process.env.SECRET,
+    })
+);
 
 
+// utilisation de nos routes
 app.use(userRouter);
 app.use(oilRouter);
 
+
+// l'écoute du serveur
 app.listen (PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
 })
