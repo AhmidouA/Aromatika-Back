778ab463c6ca13564c2a6810ef7f71252ba714f2
AhmidouA <ahmedakkioui75@gmail.com>
2023-09-07 12:26:56 +0200

    init PostgreSql in Vercel

--- server/service/dbClient.js
+++ server/service/dbClient.js
@@ -1,5 +1,7 @@
 const { Client } = require('pg');
-const client = new Client();
+const client = new Client({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 // console.log(client);
 client.connect();
 
